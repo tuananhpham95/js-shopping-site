a9d68b9073b7a12ed34a83aede1dd2ea781f8383
Tuan Anh Pham <tuananh@hotmail.se>
2023-10-04 22:29:56 +0200

    first commit

--- js/cart.js
+++ js/cart.js
@@ -1,37 +1,129 @@
-let shoppingCart = document.getElementById("shopping-cart")
+let shoppingCart = document.getElementById("shopping-cart");
+let label = document.querySelector(".label");
 
-let basket = JSON.parse(localStorage.getItem("data")) || []
-
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 // Produktdatat finns i variabeln shopData (se data.js)
 
-
 const generateCartItems = () => {
-    // Generera alla produkter med dynamisk HTML och Array.protype.map() samt join()
-    //
-    // Använd denna markup för varje produktkort - den korresponderar mot CSS:en
-    //
-    // <div class="cart-item">
-    // <img width="100" src={--image--} alt="" />
-    // <div class="details">
-    //     <div class="title-price-x">
-    //     <h4 class="title-price">
-    //         <p>{--title--}</p>
-    //         <p class="cart-item-price"> {--price--}</p>
-    //     </h4>
-    //     <i onclick="removeItem({--id--})" class="bi bi-x-lg"></i>
-    //     </div>
-    //     <div class="cart-buttons">
-    //     <div class="buttons">
-    //         <i onclick="decrement({--id--})" class="bi bi-dash-lg"></i>
-    //         <div id={--id--} class="quantity">{--total--}</div>
-    //         <i onclick="increment({--id--})" class="bi bi-plus-lg"></i>
-    //     </div>
-    //     </div>
-    //     <h3> {--total * price--}</h3>
-    // </div>
-    // </div>
- 
-}
+  // Generera alla produkter med dynamisk HTML och Array.protype.map() samt join()
+  if (basket.length !== 0) {
+    return (shoppingCart.innerHTML = basket
+      .map((product) => {
+        let { id, item } = product;
+        let search = shopData.find((x) => x.id === id) || [];
+        return `
+            <div class="cart-item">
+                <img width="100" src=${search.image} alt="" />
+            <div class="details">
+                <div class="title-price-x">
+                    <h4 class="title-price">
+                        <p>${search.title}</p>
+                        <p class="cart-item-price">${search.price}$</p>
+                    </h4>
+                    <i onclick="removeItem(${
+                      search.id
+                    })" class="bi bi-x-lg"></i>
+                </div>
+                <div class="buttons">
+                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">${item}</div>
+                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                </div>
+                <h3> ${item * search.price}$</h3>
+            </div>
+         </div>
+            `;
+      })
+      .join(""));
+  } else {
+    shoppingCart.innerHTML = "";
+    label.innerHTML = `
+        <h2>Cart is empty</h2>
+        <a href="index.html">
+            <button class="homeBtn">back to homepage</button>
+        </a>
+        `;
+  }
+};
+
+const increment = (id) => {
+  // Om användaren klickar på + på produkten
+  let search = basket.find((x) => x.id === id);
+  if (search === undefined) {
+    basket.push({
+      id: id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  generateCartItems();
+  updateCartItem(id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+const decrement = (id) => {
+  // Om användaren klickar på - på produkten
+  let search = basket.find((x) => x.id === id);
+  if (search.item === 0) {
+    return;
+  } else {
+    search.item -= 1;
+  }
+  updateCartItem(id);
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+const updateCartItem = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  totalPrice();
+  /* let total = 0
+    for(let i = 0; i < basket.length; i++){
+        const p = basket[i]
+        total += p.item;
+    }
+    return total; */
+};
+const calculation = () => {
+  let cartItem = document.getElementById("cartAmount");
+  cartItem.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+calculation();
+
+let removeItem = (id) => {
+  let basketId = id;
+  for (let i = 0; i < basket.length; i++) {
+    if (basket[i].id === basketId) {
+      basket.splice(i, 1);
+      localStorage.setItem("data", JSON.stringify(basket));
+    }
+  }
+  totalPrice();
+  calculation();
+  return generateCartItems();
+};
+const totalPrice = () => {
+  let total = 0;
+  basket.forEach((product) => {
+    let { id, item } = product;
+    let search = shopData.find((x) => x.id === id) || [];
+    total = total + search.price * item;
+    label.innerHTML = `
+            <h3>Total bills :${total} $ </h3>
+            <button class ="removeAll" onclick="clearCart()">Clear card</button>
+            `;
+  });
+};
+totalPrice();
+
+const clearCart = () => {
+  basket = [];
+  calculation();
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
 
 generateCartItems();
--- js/main.js
+++ js/main.js
@@ -1,41 +1,132 @@
-const shop = document.getElementById('shop');
- 
+const shop = document.getElementById("shop");
+
 let basket = JSON.parse(localStorage.getItem("data")) || [];
+const categories = document.getElementById("categories");
 
 // Produktdatat finns i variabeln shopData (se data.js)
+function generateShopHTML(product) {
+  let { id, image, description, title, price } = product;
+  let search = basket.find((x) => x.id === id) || {};
 
-
-const generateShop = () => {
-    // Generera alla produkter med dynamisk HTML och Array.protype.map() samt join()
-    //
-    // Använd denna markup för varje produktkort - den korresponderar mot CSS:en
-    //
-    // <div id=product-id-{--id--} class="item">
-    //     <img width="220" src={--url--} alt=""> 
-    //     <div class="details">
-    //         <h3>{--title-}</h3>
-    //         <p>{--desription--}</p>
-    //         <div class="price-quantity">
-    //         <h2>{--price-)-</h2>
-    //         <div class="buttons">
-    //             <i onclick="decrement({--id--})" class="bi bi-dash-lg"></i>
-    //             <div id={--id--} class="quantity">
-    //             </div>
-    //             <div id={--id--} class="quantity">???</div>
-    //             <i onclick="increment({--id--})" class="bi bi-plus-lg"></i>
-    //         </div>
-    //         </div>
-    //     </div>
-    // </div>
-
+  return `
+    <div id="product-id-${id}" class="item">
+      <img width="220" src="${image}" alt=""> 
+      <div class="details">
+        <h3>${title}</h3>
+        <p>${description}</p>
+        <div class="price-quantity">
+          <h2>${price}</h2>
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id="${id}" class="quantity">${
+    search.item === undefined ? 0 : search.item
+  }</div>
+            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+        </div>
+      </div>
+    </div>`;
 }
+const generateShop = () => {
+  // Generera alla produkter med dynamisk HTML och Array.protype.map() samt join()
+  shop.innerHTML = shopData
+    .map((product) => generateShopHTML(product))
+    .join("");
+};
 
-generateShop()
+generateShop();
 
 const increment = (id) => {
-    // Om användaren klickar på + på produkten 
+  // Om användaren klickar på + på produkten
+  let search = basket.find((x) => x.id === id);
+  if (search === undefined) {
+    basket.push({
+      id: id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  /* let cartItem = document.getElementById('cartAmount');
+    cartItem.innerHTML = updateCartItem(id); */
+  updateCartItem(id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+const decrement = (id) => {
+  // Om användaren klickar på - på produkten
+  let search = basket.find((x) => x.id === id);
+  if (search.item === 0) {
+    return;
+  } else {
+    search.item -= 1;
+  }
+  updateCartItem(id);
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+const updateCartItem = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  /* let total = 0
+    for(let i = 0; i < basket.length; i++){
+        const p = basket[i]
+        total += p.item;
+    }
+    return total; */
+};
+const calculation = () => {
+  let cartItem = document.getElementById("cartAmount");
+  cartItem.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+calculation();
+
+let productCategories = [
+  ...new Set(shopData.map((product) => product.category)),
+];
+
+function generateCategoryButtons() {
+  let buttonsContainer = document.getElementById("categories");
+  //skapa en knapp för alla categories
+  let allCategoriesButton = document.createElement("button");
+  allCategoriesButton.innerText = "All categories";
+  allCategoriesButton.addEventListener("click", function () {
+    filterProducts("All categories");
+  });
+  buttonsContainer.appendChild(allCategoriesButton);
+
+  // Skapa en knapp för varje kategori
+  productCategories.forEach(function (category) {
+    let button = document.createElement("button");
+    button.innerText = category;
+    button.addEventListener("click", function () {
+      filterProducts(category);
+    });
+    buttonsContainer.appendChild(button);
+  });
 }
 
-const decrement = (id) => {
-    // Om användaren klickar på - på produkten 
+function filterProducts(category) {
+  let productListContainer = document.getElementById("shop");
+
+  // Rensa befintliga produkter
+  productListContainer.innerHTML = "";
+
+  // Filtrera produkter baserat på kategori
+  let filteredProducts =
+    category === "All categories"
+      ? shopData
+      : shopData.filter(function (product) {
+          return product.category === category;
+        });
+
+  // Skapa element för filtrerade produkter
+  filteredProducts.forEach(function (product) {
+    let productElement = document.createElement("div");
+
+    productElement.innerHTML = generateShopHTML(product);
+    productListContainer.appendChild(productElement);
+  });
 }
+
+generateCategoryButtons();
